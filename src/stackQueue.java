5e065acca8ab56bada42590183db7656d1e6542a
apenny23 <apenny23@kentdenver.org>
2022-01-25 18:40:40 -0700

    Added comments and the driver method

--- src/stackQueue.java
+++ src/stackQueue.java
@@ -1,16 +1,16 @@
 
 public class stackQueue<E,T> {
-    private int length=0;
-    private StackNode stack1;
-    private StackNode stack2;
+    private int length=0;//tracks length
+    private StackNode stack1;//main stack
+    private StackNode stack2;//stack that the values are moved to in order to dequeue
     private T tmp;
 
-    public stackQueue(){
+    public stackQueue(){ //defsault construcor
         stack1=new StackNode();
         stack2=new StackNode();
     }
 
-    public void enqueue(E element){
+    public void enqueue(E element){ //adds element on to the top of the stack1
         stack1.push(element);
         length++;
 
@@ -20,29 +20,29 @@ public class stackQueue<E,T> {
 
     public T deQueue(){
 
-        if(length==0){
+        if(length==0){//returns null if it is empty
             tmp=null;
         }
-        if(length==1){
+        if(length==1){//if it is only one value, retuens that value and empties the stack
             length--;
             tmp=(T)stack1.pop();
         }
         if(length>1){
 
-            for(int a=1;a<length;a++){
+            for(int a=1;a<length;a++){//shifts everything to stack 2
                 stack2.push(stack1.pop());
 
             }
 
-            tmp=(T)stack1.pop();
+            tmp=(T)stack1.pop();//takes the botton value of stsck 1 and removes it
 
-            for(int i=1;i<length;i++){
+            for(int i=1;i<length;i++){//moves all the vaules back to stack 1 and resets it
                 stack1.push(stack2.pop());
             }
 
 
 
-            if(length>0){
+            if(length>0){//decrements the length if applicable
                 length--;
             }
 
@@ -53,7 +53,7 @@ public class stackQueue<E,T> {
 
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty(){//returns true if empty and false if it has a value in it
         if (stack1.isEmpty()&&stack2.isEmpty()&&length==0){
             return true;
         }
@@ -61,27 +61,27 @@ public class stackQueue<E,T> {
     }
 
 
-    public int size(){
+    public int size(){//returns the lenght int
         return length;
     }
 
     public T peek(){
-        if(length==0){
+        if(length==0){//if empty, returns null
             tmp=null;
         }
-        if(length==1){
+        if(length==1){//if the length is one, it returns that value without removing it
             tmp=(T)stack1.peek();
         }
         if(length>1){
 
-            for(int a=1;a<length;a++){
+            for(int a=1;a<length;a++){//moves everything to secoind stack
                 stack2.push(stack1.pop());
 
             }
 
-            tmp=(T)stack1.peek();
+            tmp=(T)stack1.peek();//copys the last value in stack one
 
-            for(int i=1;i<length;i++){
+            for(int i=1;i<length;i++){//moves back to the first stack
                 stack1.push(stack2.pop());
             }
 
@@ -103,10 +103,29 @@ public class stackQueue<E,T> {
 
         s.enqueue("four");
         s.enqueue(5);
-        s.enqueue(6);
+        s.enqueue(null);
         s.enqueue("seven");
 
-        
+        System.out.println("is empty? " + s.isEmpty());
+        System.out.println("Size: "+ s.size());
+        System.out.println(s.deQueue());
+        System.out.println(s.deQueue());
+        System.out.println("testing peek twice");
+        System.out.println(s.peek());
+        System.out.println(s.peek());
+        System.out.println("Continuing to dequeue the enqueued values");
+        System.out.println(s.deQueue());
+        System.out.println(s.deQueue());
+        System.out.println(s.deQueue());
+        System.out.println(s.deQueue());
+        System.out.println(s.deQueue());
+        System.out.println("is empty? " + s.isEmpty());
+        System.out.println("Size: "+ s.size());
+
+
+
+
+
 
 
 
